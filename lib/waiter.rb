992181d5b256d6fff3fd15b3da3bedd7d04f9c3c
Sonja Parsell <hello@sparsell.com>
2020-05-12 18:59:35 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,9 +22,9 @@ class Waiter
   end
 
   def best_tipper
-    best_tip = meals.max do |meal_a, meal_b| 
+    best_tip = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
-      waiter.customer.best_tipper
+      self.best_tipper
     end
   end
 
