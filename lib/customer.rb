7a6339ea752aebb17ee0909a235857c73ec40cae
Sonja Parsell <hello@sparsell.com>
2020-05-12 18:49:27 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -24,7 +24,7 @@ class Customer
 
   def waiters
 #returns an Array of Waiter instances associated with this customer's meals
-  meals.map {|meal| meal.waiter}
+  meals.select {|meal| meal.waiter}
   end
 
 end
